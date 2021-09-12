965abb41bcd58677eadcf48201b84e5dd6e13161
Alan <ageiersigal@gmail.com>
2021-09-11 23:08:55 -0300

    Conexiones y estilos

---
+++ data/usuarios.js
@@ -0,0 +1,26 @@
+const usuariosData = {
+    lista: [
+            {
+                nombre:'Alan',
+                apellido:'Geier',
+                usuario: 'a.geier',
+                email:'ageiersigal@gmail.com'
+            },{
+                
+                    nombre:'Facundo',
+                    apellido:'Torrez',
+                    usuario: 'ftorrez',
+                    email:'ftorrez@gmail.com'
+                },{
+                nombre:'Mauro',
+                apellido:'Proto',
+                usuario: 'm_proto',
+                email:'mproto@gmail.com'
+                },{
+                    nombre:'Esteban',
+                    apellido:'Rodriguez',
+                    usuario: 'Elesteban',
+                    email:'esterodriguez@gmail.com'
+                }
+    ]
+            }
--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,11 @@
 var express = require('express');
 var router = express.Router();
-const usersController = require('../controllers/usersControllers')
-
+const usersController = require('../controllers/usersControllers');
 //router.get('/', usersController.index)
-//router.get('/ususarios/:id',falsebookController.id)
-
+//router.get('/usuarios/:id',falsebookController.id)
+router.get('/registracion',usersController.registracion);
+router.get('/login',usersController.login);
+router.get('/miPerfil',usersController.miPerfil);
+router.get('/editarPerfil',usersController.editarPerfil);
+router.get('/detalleUsuario',usersController.detalleUsuario);
 module.exports = router;
