c42cecd4233cac72507bf4672a493c5b0d74f634
Facundotorrez <64669528+Facundotorrez@users.noreply.github.com>
2021-11-24 22:32:25 -0300

    Extras
    Follow and Unfollow

--- controllers/usersControllers.js
+++ controllers/usersControllers.js
@@ -16,7 +16,7 @@ miPerfil: async function (req,res) {
         res.send('NO EXISTE EL USUARIO')
     }
     const user = await db.usuarios.findByPk(req.session.user.id_usuario, {
-        include: [{association: 'posts', order: [['posts','id_usuario', 'desc']] }] 
+        include: [{association: 'posts'}]
     });
   
     res.render('miPerfil', {user});
@@ -53,10 +53,35 @@ detalleUsuario: async function (req,res) {
 
 },  
 
+follow: function (req, res) {
+    if (!req.session.usuarios){
+      res.redirect('/usuarios/'+req.params.id); 
+    }
+    db.Follow.create ({ 
+      Follower_id: req.session.usuarios.id,
+      Following_id: req.params.id
+    }).then(follow => {
+      res.redirect('/usuarios'+req.params.id);
+    }).catch(error => {
+      return res.send(error);
+    })
+},
+
+unfollow: function (req, res) {
+  if (!req.session.usuarios){
+    res.redirect('/usuarios/'+req.params.id);
+  }
+  db.Follow.destroy (
+    { where : { follower_id: req.session.usuarios.id , Following_id: req.params.id}
+  }).then(Follow => {
+    res.redirect('/usuarios'+req.params.id);
+}).catch(error => {
+  return res.send(error);
+})
+}
 
 
-
-
+//-----------------------------------------------------------------------------------------------------
 
 // login: function (req,res) { 
 //     console.log(req.body); // aca voiy a poner el contenido del form     
